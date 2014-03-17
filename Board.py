283da28c8da8ddce3cde03a6d4157361e677ccc1
carl <thunman@gmail.com>
2014-03-17 09:06:17 -0400

    just getting started on this

--- Board.py
+++ Board.py
@@ -1,7 +1,19 @@
+import string
 
 class Board:
+
+	i = 12345
+
+	def f(self):
+		return 'hello world'
+
+	def initializeBoard(self, fen_string):
+		rows = string.split(fen_string, '/')
+		for row in rows:
+			print row
 
 	def __init__(self):
 		"""do nothing"""
 
+"""rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"""
 	
