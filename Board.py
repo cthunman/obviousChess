6d5a8e749588ab01ff59ab057c0e401effc24e0a
carl <thunman@gmail.com>
2014-04-10 22:40:45 -0400

    starting to work on fen notation

--- Board.py
+++ Board.py
@@ -2,18 +2,23 @@ import string
 
 class Board:
 
-	i = 12345
-
-	def f(self):
-		return 'hello world'
-
-	def initializeBoard(self, fen_string):
-		rows = string.split(fen_string, '/')
-		for row in rows:
-			print row
-
 	def __init__(self):
-		"""do nothing"""
+		self.board = []
+		self.board.append([0, 0, 0, 0, 0, 0, 0, 0,])
+		self.board.append([0, 0, 0, 0, 0, 0, 0, 0,])
+		self.board.append([0, 0, 0, 0, 0, 0, 0, 0,])
+		self.board.append([0, 0, 0, 0, 0, 0, 0, 0,])
+		self.board.append([0, 0, 0, 0, 0, 0, 0, 0,])
+		self.board.append([0, 0, 0, 0, 0, 0, 0, 0,])
+		self.board.append([0, 0, 0, 0, 0, 0, 0, 0,])
+		self.board.append([0, 0, 0, 0, 0, 0, 0, 0,])
+
+	def initialize_board(self, fen_string):
+		rows = string.split(fen_string, '/')
+		for row_index, row in enumerate(rows):
+			# print row
+			for index, character in enumerate(row):
+				self.board[row_index][index] = character
 
 """rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"""
 	
---
+++ test.py
@@ -0,0 +1,6 @@
+from Board import Board
+
+board = Board()
+
+board.initialize_board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+print board.board
