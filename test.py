fab50b183f50faf5d1d81e606e537612e21751f2
carl <thunman@gmail.com>
2014-04-13 14:07:59 -0400

    working on FEN compatibility

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ from Board import Board
 
 board = Board()
 
-board.initialize_board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
-print board.board
+# board.initialize_board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR')
+board.initialize_board('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR')
+print unicode(board)
